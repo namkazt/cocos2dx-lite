28672ee0ff05eaab66effe4ed80a499522bfbdea
yinjimmy <jimmy.yin5@gmail.com>
2015-10-16 11:28:54 +0800

    cleanup

---
+++ clean.sh
@@ -0,0 +1,6 @@
+rm -fr `find . -name "*linux*"`
+rm -fr `find . -name "*win10*"`
+rm -fr `find . -name "*winrt*"`
+rm -fr `find . -name "*win8*"`
+rm -fr `find . -name "*wp_8.1*"`
+rm -fr `find . -name "CMakeLists.txt"`
