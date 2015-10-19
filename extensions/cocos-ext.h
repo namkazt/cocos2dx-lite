41773d24c723041f099f8f8c0360b80f638879e9
yinjimmy <jimmy.yin5@gmail.com>
2015-10-19 13:00:01 +0800

    clean

--- extensions/cocos-ext.h
+++ extensions/cocos-ext.h
@@ -18,8 +18,4 @@
 
 #include "ExtensionDeprecated.h"
 
-// Particle System, include Particle Universe Particle System
-#include "Particle3D/CCParticleSystem3D.h"
-#include "Particle3D/PU/CCPUParticleSystem3D.h"
-
 #endif /* __COCOS2D_EXT_H__ */
